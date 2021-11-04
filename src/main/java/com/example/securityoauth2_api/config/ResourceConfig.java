d39df76f332b924b22b4bb8e2b48598623bb4a7c
AkkeSun <akkessun@gmail.com>
2021-11-04 13:51:13 +0900

    header에 토큰 심어 보내기

--- src/main/java/com/example/securityoauth2_api/config/ResourceConfig.java
+++ src/main/java/com/example/securityoauth2_api/config/ResourceConfig.java
@@ -32,6 +32,7 @@ public class ResourceConfig  extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/").permitAll()
                 .antMatchers("/v1/users").hasAnyRole("ADMIN")
                 .antMatchers("/v1/user").hasAnyRole("ADMIN")
                 .anyRequest().authenticated();
